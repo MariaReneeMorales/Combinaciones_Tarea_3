c3e7906eadeb88c9621fd34ff68055e002a3272a
María Reneé Morales <99778960+MariaReneeMorales@users.noreply.github.com>
2022-02-16 09:01:57 -0600

    Update Combinaciones - Tarea 3.cpp

--- Combinaciones - Tarea 3.cpp
+++ Combinaciones - Tarea 3.cpp
@@ -11,11 +11,13 @@ int main() {
 	setlocale(LC_ALL, "");
 	
 	//Definición de variables.
+	string lcUser, lcPass;
 	string lcOpcion;
 	float lnNumero1, lnNumero2, lnResultado, lnSaldo, lnValor, lnSaldoInicial, lnIngresos, lnEgresos;
-	int lnCanIng, lnCanEgr;
+	int lnCanIng, lnCanEgr, lnCount;
 	
 	//Inicializando valores.
+	lnCount = 3;
 	lnCanIng = 0;
 	lnCanEgr = 0;
 	lnIngresos = 0;
@@ -24,107 +26,137 @@ int main() {
 	lnSaldo = lnSaldoInicial;
 	lnValor = 0;
 	
-	//Inicio del ciclo do-while (se repite hasta que el usuario presioné "S" para salir).
-	do{
-		//Mostrando en pantalla la (opción a realizar) + Captura y Lectura de datos.
-		cout << " ¿Qué desea hacer?" << endl << endl; 
-		cout << " S = Sumar, R = Restar, M = Multiplicar, D = Dividir, F = Facturar, P = Pagar, Z = Salir" << endl << endl;
-		cout << " Escoga una opción: ";
-		cin >> lcOpcion;
+	//Inicio del ciclo do-while (permite hasta 3 intentos fallidos).
+	do {
+		//Captura y lectura de datos (tomando el nombre del usuario y contraseña).
+		cout << "Usuario: "; 
+		cin >> lcUser;
+		cout << "Contraseña: ";
+		cin >> lcPass;
 		
-		//Conjunto de condiciones para determinar (opción de usuario) + Operaciones correspondientes.
-		if (lcOpcion == "S"){
-			cout << " Ingrese el primer número: ";
-			cin >> lnNumero1;
-			cout << " Ingrese el segundo número: ";
-			cin >> lnNumero2;
+		//Inicio de condición if (evitando que se ingresen credenciales incorrectas).
+		if (lcUser == "Maria" && lcPass == "140102"){
+			lnCount = 0; //Forzando la salida del sistema.
 			
-			lnResultado = (lnNumero1 + lnNumero2);
+			//Dándole la bienvenida al usuario + nota importante.
+			cout << endl;
+			cout << "¡Bienvenida " << lcUser << "! " << endl;
 			
-			cout << " El resultado de " << lnNumero1 << " + " << lnNumero2 << " = " << lnResultado << endl << endl;
-		}
-		else if (lcOpcion == "R"){
-			cout << " Ingrese el primer número: ";
-			cin >> lnNumero1;
-			cout << " Ingrese el segundo número: ";
-			cin >> lnNumero2;
-			
-			lnResultado = (lnNumero1 - lnNumero2);
-			
-			cout << " El resultado de " << lnNumero1 << " - " << lnNumero2 << " = " << lnResultado << endl << endl;
-		}
-		else if (lcOpcion == "M"){
-			cout << " Ingrese el primer número: ";
-			cin >> lnNumero1;
-			cout << " Ingrese el segundo número: ";
-			cin >> lnNumero2;
-			
-			lnResultado = (lnNumero1 * lnNumero2);
-			
-			cout << " El resultado de " << lnNumero1 << " * " << lnNumero2 << " = " << lnResultado << endl << endl;
+			//Inicio del ciclo do-while (se repite hasta que el usuario presioné "S" para salir).
+			do{
+				//Mostrando en pantalla el saldo actual + captura y lectura (opción a realizar).
+				cout << " ¿Qué desea hacer?" << endl << endl; 
+				cout << " S = Sumar, R = Restar, M = Multiplicar, D = Dividir, F = Facturar, P = Pagar, Z = Salir" << endl << endl;
+				cout << " Escoga una opción: ";
+				cin >> lcOpcion;
+				
+				if (lcOpcion == "S"){
+					cout << " Ingrese el primer número: ";
+					cin >> lnNumero1;
+					cout << " Ingrese el segundo número: ";
+					cin >> lnNumero2;
+					
+					lnResultado = (lnNumero1 + lnNumero2);
+					
+					cout << " El resultado de " << lnNumero1 << " + " << lnNumero2 << " = " << lnResultado << endl << endl;
+				}
+				else if (lcOpcion == "R"){
+					cout << " Ingrese el primer número: ";
+					cin >> lnNumero1;
+					cout << " Ingrese el segundo número: ";
+					cin >> lnNumero2;
+					
+					lnResultado = (lnNumero1 - lnNumero2);
+					
+					cout << " El resultado de " << lnNumero1 << " - " << lnNumero2 << " = " << lnResultado << endl << endl;
+				}
+				else if (lcOpcion == "M"){
+					cout << " Ingrese el primer número: ";
+					cin >> lnNumero1;
+					cout << " Ingrese el segundo número: ";
+					cin >> lnNumero2;
+					
+					lnResultado = (lnNumero1 * lnNumero2);
+					
+					cout << " El resultado de " << lnNumero1 << " * " << lnNumero2 << " = " << lnResultado << endl << endl;
+				}	
+				else if (lcOpcion == "D"){
+					cout << " Ingrese el primer número: ";
+					cin >> lnNumero1;
+					cout << " Ingrese el segundo número: ";
+					cin >> lnNumero2;
+					
+					lnResultado = (lnNumero1 / lnNumero2);
+					
+					if (lnNumero2 == 0){
+						cout << " El resultado de " << lnNumero1 << " / " << lnNumero2 << " = Math Error..." << endl << endl;
+					}
+					else{
+						cout << " El resultado de " << lnNumero1 << " / " << lnNumero2 << " = " << lnResultado << endl << endl;
+					}
+				}
+				else if (lcOpcion == "F"){
+					//Captura y lectura de datos (valor dado).
+					cout << " Nota: Su saldo actual es de: USD " << lnSaldo << endl << endl;
+					cout << " Ingrese la cantidad: USD "; 
+					cin >> lnValor;
+							
+					//Suma (nuevos ingresos).
+					lnSaldo = lnSaldo + lnValor;
+							
+					//Conteo n de facturas + n ingresos.
+					lnCanIng = lnCanIng + 1;
+					lnIngresos = lnIngresos + lnValor;
+				}
+				else if (lcOpcion == "P"){
+					//Captura y lectura de datos (valor dado).
+					cout << " Nota: Su saldo actual es de: USD " << lnSaldo << endl << endl;
+					cout << " Ingrese la cantidad: USD "; 
+					cin >> lnValor;
+							
+					//Condición (evitando que se proceda si el monto es < saldo actual).
+					if (lnValor > lnSaldo){
+						cout << " Su saldo es insuficiente";
+						cout << endl;
+					}
+					else {
+						//Resta (pagos trabajadores).
+						lnSaldo = lnSaldo - lnValor;
+								
+						//Conteo n de pagos + n egresos.
+						lnCanEgr = lnCanEgr + 1;
+						lnEgresos = lnEgresos + lnValor;
+					}
+				}
+				cout << "----------------------------------------------------------------------------------------------------------" << endl;
+			} while (lcOpcion == "S" || lcOpcion == "R" || lcOpcion == "M" || lcOpcion == "D" || lcOpcion == "F" || lcOpcion == "P");
+		 
+			//Título. 
+			cout << endl << endl;
+			cout << "===================" << endl;
+			cout << " ¡Resumen del Día!" << endl;
+			cout << "===================" << endl << endl; 
+				
+			//Impresión de datos.
+			cout << " Saldo inicial: USD " << lnSaldoInicial << endl;
+			cout << " (+) " << lnCanIng << " Facturas por USD " << lnIngresos << endl;
+			cout << " (-) " << lnCanEgr << " Pagos por USD " << lnEgresos << endl;
+			cout << " Saldo final: USD " << lnSaldo;
 		}	
-		else if (lcOpcion == "D"){
-			cout << " Ingrese el primer número: ";
-			cin >> lnNumero1;
-			cout << " Ingrese el segundo número: ";
-			cin >> lnNumero2;
+		else {
+			//Se resta la cantidad n de intentos.
+			lnCount = lnCount - 1;
 			
-			lnResultado = (lnNumero1 / lnNumero2);
+			//Mostrando al usuario los intentos que le sobran.
+			cout << "Credenciales incorrectas...Le quedan " << lnCount << " intentos." << endl << endl;
+			cout << "-------------------------------------------------" << endl;
 			
-			if (lnNumero2 == 0){
-				cout << " El resultado de " << lnNumero1 << " / " << lnNumero2 << " = Math Error..." << endl << endl;
-			}
-			else{
-				cout << " El resultado de " << lnNumero1 << " / " << lnNumero2 << " = " << lnResultado << endl << endl;
+			//Condición (mensaje de seguridad cuando la n intentos finaliza).
+			if (lnCount == 0){
+				cout << "Por seguridad la sesión ha sido finalizada.";
 			}
 		}
-		else if (lcOpcion == "F"){
-			//Captura y lectura de datos (valor dado).
-			cout << " Nota: Su saldo actual es de: USD " << lnSaldo << endl << endl;
-			cout << " Ingrese la cantidad: USD "; 
-			cin >> lnValor;
-					
-			//Suma (nuevos ingresos).
-			lnSaldo = lnSaldo + lnValor;
-					
-			//Conteo n de facturas + n ingresos.
-			lnCanIng = lnCanIng + 1;
-			lnIngresos = lnIngresos + lnValor;
-		}
-		else if (lcOpcion == "P"){
-			//Captura y lectura de datos (valor dado).
-			cout << " Nota: Su saldo actual es de: USD " << lnSaldo << endl << endl;
-			cout << " Ingrese la cantidad: USD "; 
-			cin >> lnValor;
-					
-			//Condición (evitando que se proceda si el monto es < saldo actual).
-			if (lnValor > lnSaldo){
-				cout << " Su saldo es insuficiente";
-				cout << endl;
-			}
-			else {
-				//Resta (pagos trabajadores).
-				lnSaldo = lnSaldo - lnValor;
-						
-				//Conteo n de pagos + n egresos.
-				lnCanEgr = lnCanEgr + 1;
-				lnEgresos = lnEgresos + lnValor;
-			}
-		}
-		cout << "----------------------------------------------------------------------------------------------------------" << endl;
-	} while (lcOpcion == "S" || lcOpcion == "R" || lcOpcion == "M" || lcOpcion == "D" || lcOpcion == "F" || lcOpcion == "P");
- 
-	//Título. 
-	cout << endl << endl;
-	cout << "===================" << endl;
-	cout << " ¡Resumen del Día!" << endl;
-	cout << "===================" << endl << endl; 
-		
-	//Impresión de datos.
-	cout << " Saldo inicial: USD " << lnSaldoInicial << endl;
-	cout << " (+) " << lnCanIng << " Facturas por USD " << lnIngresos << endl;
-	cout << " (-) " << lnCanEgr << " Pagos por USD " << lnEgresos << endl;
-	cout << " Saldo final: USD " << lnSaldo;
+	} while (lnCount > 0);
 	
 	return 0;
 }
